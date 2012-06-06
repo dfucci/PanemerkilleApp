b1c0e282129cf7c30f0cd49b2f1e1208324547f4
slacko <ancestrall@gmail.com>
2012-06-06 18:47:17 +0300

    added facebook logout

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //var DEBUG="http://localhost:7777";
-var endpoint =  "http://blazing-mist-8758.herokuapp.com";
+var endpoint = "http://blazing-mist-8758.herokuapp.com";
 var user = {};
 $('#yourpatches').live('pageshow', function(event) {
     console.log('fired');
@@ -12,73 +12,60 @@ $('#yourcheckins').live('pageshow', function(event) {
     var id = getUrlVars()["user"];
     $.getJSON(endpoint + '/users/' + id, displayCheckins);
 });
-function displayCheckins (data) {
+
+function displayCheckins(data) {
     var checkins = data.checkins;
-    var blocks=['a','b','c','d'];
+    var blocks = ['a', 'b', 'c', 'd'];
     $.each(checkins, function(index, c) {
         var event_name = c.event.name;
         var event_poster = c.event.poster_url;
-        var k=index%4;
-        var cls ="ui-block-" + blocks[k];
-        $('#checkingrid').append('<div class='+cls+'> <div><img src="' + event_poster + '"></div></div>');
+        var k = index % 4;
+        var cls = "ui-block-" + blocks[k];
+        $('#checkingrid').append('<div class=' + cls + '> <div><img class=poster src="' + event_poster + '"></div></div>');
     });
 }
+
 function displayPatches(data) {
     var patches = data.patches;
-    var blocks=['a','b','c','d'];
+    var blocks = ['a', 'b', 'c', 'd'];
     $.each(patches, function(index, p) {
         var patch_name = p.patch.name;
         var patch_image = p.patch.image_url;
-        var k=index%4;
-        var cls ="ui-block-" + blocks[k];
-        $('#patchgrid').append('<div class='+cls+'> <div><img src="' + patch_image + '"></div><div>' + patch_name + '</div> </div>');
+        var k = index % 4;
+        var cls = "ui-block-" + blocks[k];
+        $('#patchgrid').append('<div class=' + cls + '> <div><img class=patch src="' + patch_image + '"></div><div class=patch>' + patch_name + '</div> </div>');
     });
 }
 
 
 //TODO: sostiture ready con phonegap.ondeviceready
-$('#profile').live('pagebeforecreate',function(){ //TODO: carica l'id quando phonegap è pronto
-    if (user.id == undefined) {//TODO: controlla lo storage per l'id
-    facebook_id='641892040';
-    $.getJSON(endpoint+"/users",{
-         facebook_id : facebook_id
-      }, function (data) {
-        if (data){
-            user.id=data[0]._id;
-            populateUser();
-        }
-    });
-}
+$('#profile').live('pagebeforecreate', function() { //TODO: carica l'id quando phonegap è pronto
+    if (user.id == undefined) { //TODO: controlla lo storage per l'id
+        facebook_id = '641892040';
+        $.getJSON(endpoint + "/users", {
+            facebook_id: facebook_id
+        }, function(data) {
+            if (data) {
+                user.id = data[0]._id;
+                populateUser();
+            }
+        });
+    }
+    
 });
 
-
-// $('#profile').on('deviceready',function(){ //TODO: carica l'id quando phonegap è pronto
-//     if (user.id == undefined) {//TODO: controlla lo storage per l'id
-//     facebook_id='641892040';
-//     $.getJSON(endpoint+"/users",{
-//          facebook_id : facebook_id
-//       }, function (data) {
-//         if (data){
-//             user.id=data[0]._id;
-//         }
-//     });
-//     }
-// });
-
-
-function populateUser(){
-    $.getJSON(endpoint+"/users/" + user.id, function(data) {
-         $("#picture_url").attr("src", data.picture_url);
-         $("#firstname").html(data.name.firstname);
-         $("#surname").html(data.name.surname);
-         $("#patch_counter").html(data.patches.length);
-         $("#checkin_counter").html(data.checkins.length);
-         $("#userPatchesLink").attr('href', 'userPatches.html?user='+user.id);
-         $("#userCheckinLink").attr('href', 'userCheckins.html?user='+user.id);
+function populateUser() {
+    $.getJSON(endpoint + "/users/" + user.id, function(data) {
+        $("#picture_url").attr("src", data.picture_url);
+        $("#firstname").html(data.name.firstname);
+        $("#surname").html(data.name.surname);
+        $("#patch_counter").html(data.patches.length);
+        $("#checkin_counter").html(data.checkins.length);
+        $("#userPatchesLink").attr('href', 'userPatches.html?user=' + user.id);
+        $("#userCheckinLink").attr('href', 'userCheckins.html?user=' + user.id);
     });
 }
-    
- 
+
 
 
 function getUrlVars() {
