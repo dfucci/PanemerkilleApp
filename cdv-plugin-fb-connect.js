893ec310156e034eec082ed991f8575ea7492d35
Daniele Zanni <syymza@gmail.com>
2012-07-07 15:24:08 +0300

    Non lo so

--- cdv-plugin-fb-connect.js
+++ cdv-plugin-fb-connect.js
@@ -1,5 +1,5 @@
- (function(){CDV = ( typeof CDV == 'undefined' ? {} : CDV );
-var cordovaRef = window.PhoneGap || window.Cordova || window.cordova;
+CDV = ( typeof CDV == 'undefined' ? {} : CDV );
+var cordova = window.cordova || window.Cordova;
 CDV.FB = {
   init: function(apiKey, fail) {
     // create the fb-root element if it doesn't exist
@@ -8,7 +8,7 @@ CDV.FB = {
       elem.id = 'fb-root';
       document.body.appendChild(elem);
     }
-    cordovaRef.exec(function() {
+    cordova.exec(function() {
     var authResponse = JSON.parse(localStorage.getItem('cdv_fb_session') || '{"expiresIn":0}');
     if (authResponse && authResponse.expirationTime) { 
       var nowTime = (new Date()).getTime();
@@ -26,7 +26,7 @@ CDV.FB = {
   },
   login: function(params, cb, fail) {
     params = params || { scope: '' };
-    cordovaRef.exec(function(e) { // login
+    cordova.exec(function(e) { // login
         if (e.authResponse && e.authResponse.expiresIn) {
           var expirationTime = e.authResponse.expiresIn === 0
           ? 0 
@@ -39,21 +39,20 @@ CDV.FB = {
     }, (fail?fail:null), 'org.apache.cordova.facebook.Connect', 'login', params.scope.split(',') );
   },
   logout: function(cb, fail) {
-    cordovaRef.exec(function(e) {
+    cordova.exec(function(e) {
       localStorage.removeItem('cdv_fb_session');
       FB.Auth.setAuthResponse(null, 'notConnected');
       if (cb) cb(e);
     }, (fail?fail:null), 'org.apache.cordova.facebook.Connect', 'logout', []);
   },
   getLoginStatus: function(cb, fail) {
-    cordovaRef.exec(function(e) {
+    cordova.exec(function(e) {
       if (cb) cb(e);
     }, (fail?fail:null), 'org.apache.cordova.facebook.Connect', 'getLoginStatus', []);
   },
   dialog: function(params, cb, fail) {
-    cordovaRef.exec(function(e) { // login
+    cordova.exec(function(e) { // login
       if (cb) cb(e);
                   }, (fail?fail:null), 'org.apache.cordova.facebook.Connect', 'showDialog', [params] );
   }
 };
-  })();
