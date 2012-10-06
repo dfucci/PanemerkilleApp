a3a8ab64447b023983c9cfe2a5bf977ddc1c0d01
Daniele Zanni <syymza@gmail.com>
2012-10-06 15:43:09 +0300

    config

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
- var endpoint = "http://api.panemerkille.fi";
- var maxDistance = 200;
- var mixpanel_token="f7967f038361e149b38783b1960daf2e";
+ var endpoint = "http://panemerkille-staginherokuapp.com";
+ var maxDistance = 20000000000;
+ var mixpanel_token="b56e06d2c0b89cdc5a2256e110ca1a58";
  var codeVersion = 9;
