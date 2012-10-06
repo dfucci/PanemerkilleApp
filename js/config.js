7b2cbf88fe546a864d8f16e98482b3a6de28c4ea
Daniele Zanni <syymza@gmail.com>
2012-10-07 01:02:27 +0300

    bug fix

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
  var endpoint = "http://panemerkille-stagin.herokuapp.com";
  var maxDistance = 20000000000;
  var mixpanel_token="b56e06d2c0b89cdc5a2256e110ca1a58";
- var codeVersion = 10;
+ var codeVersion = 11;
